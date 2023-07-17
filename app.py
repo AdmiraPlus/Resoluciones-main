46dce9b9df6e9f442a59efd7f3ca156e2be5b7c7
Leoncio R. Barrios H <leonciobarriosh@gmail.com>
2023-07-17 12:04:28 -0500

    Nuevas dependencias y debug=False

--- app.py
+++ app.py
@@ -325,4 +325,4 @@ if __name__ == '__main__':
 	app.register_error_handler(401, status_401)
 	app.register_error_handler(404, status_404)
 	csrf.init_app(app)
-	app.run(debug=True)
+	app.run(debug=False)
