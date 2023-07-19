d7b3fb14242e40764d45d2f9f63b7331955f20b6
Leoncio R. Barrios H <leonciobarriosh@gmail.com>
2023-07-19 15:26:56 -0500

    Funcionando los filtros

--- app.py
+++ app.py
@@ -49,6 +49,49 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
+def convertirfecha(fecha_str):
+    # Obtener el día, mes y año de la cadena
+    dia_str, mes_str, anio_str = fecha_str.split(' de ')
+
+    # Mapear el nombre del mes a su número correspondiente
+    meses = {
+        'Enero': 1,
+        'enero': 1,
+        'Febrero': 2,
+        'febrero': 2,
+        'Marzo': 3,
+        'marzo': 3,
+        'Abril': 4,
+        'abril': 4,
+        'Mayo': 5,
+        'mayo': 5,
+        'Junio': 6,
+        'junio': 6,
+        'Julio': 7,
+        'julio': 7,
+        'Agosto': 8,
+        'agosto': 8,
+        'Septiembre': 9,
+        'septiembre': 9,
+        'Octubre': 10,
+        'octubre': 10,
+        'Noviembre': 11,
+        'noviembre': 11,
+        'Diciembre': 12,
+        'diciembre': 12
+    }
+
+    # Convertir los componentes de fecha a enteros
+    dia = int(dia_str)
+    mes = meses[mes_str]
+    anio = int(anio_str)
+
+    # Crear el objeto de fecha
+    fecha = datetime.datetime(anio, mes, dia)
+
+    # Retorna la fecha
+    return fecha.date()
+
 # Modelo de datos de Usuario
 class Usuario(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
@@ -209,137 +252,152 @@ def usuarios_eliminar(id):
 @app.route('/cargaresol', methods=['GET', 'POST'])
 def cargaresol():
     # Obtener el valor de la UIT ingresado por el usuario
-	uit = 4950
+    uit = 4950
 
-	# Obtener la lista de archivos PDF en el directorio
-	PATH  = os.path.join(app.root_path, "resoluciones")
-	files = []
+    # Obtener la lista de archivos PDF en el directorio
+    PATH = os.path.join(app.root_path, "resoluciones")
+    files = []
 
-	for dirpath, dirnames, filenames in os.walk(PATH):
-		for filename in filenames:
-			if filename.endswith('.pdf'):
-				files.append(os.path.join(dirpath, filename))
+    for dirpath, dirnames, filenames in os.walk(PATH):
+        for filename in filenames:
+            if filename.endswith('.pdf'):
+                files.append(os.path.join(dirpath, filename))
 
-	fresolucion = []
-	nresolucion = []
-	nombre = []
-	infraccion = []
-	valor = []
-	acta = []
-	fechaActa = []
-	placa = []
-	servicio = []
+    fresolucion = []
+    nresolucion = []
+    nombre = []
+    infraccion = []
+    valor = []
+    acta = []
+    fechaActa = []
+    placa = []
+    servicio = []
 
-	for filename in files:
-		f_resolucion = ""
-		n_resolucion = ""
-		nombre_rs = ""
-		t_infraccion = ""
-		valor_m = ""
-		c_acta = ""
-		fecha_Acta = ""
-		n_placa = ""
-		t_servicio = ""
+    for filename in files:
+        f_resolucion = ""
+        n_resolucion = ""
+        nombre_rs = ""
+        t_infraccion = ""
+        valor_m = ""
+        c_acta = ""
+        fecha_Acta = ""
+        n_placa = ""
+        t_servicio = ""
 
-		with pdfplumber.open(filename) as pdf:
-			for page in pdf.pages:
-				text = page.extract_text()
-				lines = text.split('\n')
+        with pdfplumber.open(filename) as pdf:
+            for page in pdf.pages:
+                text = page.extract_text()
+                lines = text.split('\n')
 
-				for line in lines:
-					if 'Trujillo, ' in line:
-						f_resolucion = line.split('Trujillo, ')[1].strip()
+                for line in lines:
+                    if 'Trujillo, ' in line:
+                        f_resolucion = line.split('Trujillo, ')[1].strip()
 
-				# Obtener el nombre del archivo sin la extensión .pdf
-				nombre_archivo = os.path.splitext(os.path.basename(filename))[0]
+                # Obtener el nombre del archivo sin la extensión .pdf
+                nombre_archivo = os.path.splitext(
+                    os.path.basename(filename))[0]
 
-				# Utilizar expresión regular para extraer el número de resolución y el nombre o razón social del archivo
-				matches_nombre = re.search(r"(?:R\.G\.R\.-)?(\d+-\d+)-(.+)", nombre_archivo)
-				if matches_nombre:
-					n_resolucion = matches_nombre.group(1)
-					nombre_rs = matches_nombre.group(2).strip().split(',')[0]
+                # Utilizar expresión regular para extraer el número de resolución y el nombre o razón social del archivo
+                matches_nombre = re.search(
+                    r"(?:R\.G\.R\.-)?(\d+-\d+)-(.+)", nombre_archivo)
+                if matches_nombre:
+                    n_resolucion = matches_nombre.group(1)
+                    nombre_rs = matches_nombre.group(2).strip().split(',')[0]
 
-				# Eliminar la parte " - copia (5)" del nombre
-				nombre_rs = re.sub(r'\s*-\s*copia\s*\(\d+\)\s*', '', nombre_rs)
+                # Eliminar la parte " - copia (5)" del nombre
+                nombre_rs = re.sub(r'\s*-\s*copia\s*\(\d+\)\s*', '', nombre_rs)
 
-				for line in lines:
-					if 'tipificada con Código ' in line:
-						matches_infraccion = re.search(r'tipificada con Código ([A-Za-z]\.[0-9])', line)
-						if matches_infraccion:
-							t_infraccion = matches_infraccion.group(1)
-						else:
-							t_infraccion = ""
+                for line in lines:
+                    if 'tipificada con Código ' in line:
+                        matches_infraccion = re.search(
+                            r'tipificada con Código ([A-Za-z]\.[0-9])', line)
+                        if matches_infraccion:
+                            t_infraccion = matches_infraccion.group(1)
+                        else:
+                            t_infraccion = ""
 
-					if 'equivalente a' in line and 'de la UIT' in line:
-						matches_valor = re.search(r"equivalente a ([\d.]+(?:\.\d+)?) de la UIT", line)
-						valor_m = str(round(float(matches_valor.group(1)) * uit)) if matches_valor else ""
+                    if 'equivalente a' in line and 'de la UIT' in line:
+                        matches_valor = re.search(
+                            r"equivalente a ([\d.]+(?:\.\d+)?) de la UIT", line)
+                        valor_m = str(
+                            round(float(matches_valor.group(1)) * uit)) if matches_valor else ""
 
-					if 'El Acta de Control ' in line:
-						c_acta = line.split('El Acta de Control ')[1].split(',')[0].replace('N°', '')
+                    if 'El Acta de Control ' in line:
+                        c_acta = line.split('El Acta de Control ')[
+                            1].split(',')[0].replace('N°', '')
 
-					if 'de fecha ' in line:
-						fecha_match = re.search(r'\d+ de \w+ de \d+', line)
-						if fecha_match:
-							fecha_Acta = fecha_match.group(0)
-						else:
-							fecha_Acta = ""
+                    if 'de fecha ' in line:
+                        fecha_match = re.search(r'\d+ de \w+ de \d+', line)
+                        if fecha_match:
+                            fecha_Acta = fecha_match.group(0)
+                        else:
+                            fecha_Acta = ""
 
-					matches_placa = re.search(r"(placa de rodaje|unidad vehicular) ([A-Z0-9-]+)", line)
-					if matches_placa:
-						n_placa = matches_placa.group(2).replace(',', '')
+                    matches_placa = re.search(
+                        r"(placa de rodaje|unidad vehicular) ([A-Z0-9-]+)", line)
+                    if matches_placa:
+                        n_placa = matches_placa.group(2).replace(',', '')
 
-					if 'servicio: ' in line:
-						t_servicio = line.split('Modalidad de servicio: ')[1].strip()
+                    if 'servicio: ' in line:
+                        t_servicio = line.split(
+                            'Modalidad de servicio: ')[1].strip()
 
-		fresolucion.append(f_resolucion)
-		nresolucion.append(n_resolucion)
-		nombre.append(nombre_rs)
-		infraccion.append(t_infraccion)
-		valor.append(valor_m)
-		acta.append(c_acta)
-		fechaActa.append(fecha_Acta)
-		placa.append(n_placa)
-		servicio.append(t_servicio)
+        fresolucion.append(f_resolucion)
+        nresolucion.append(n_resolucion)
+        nombre.append(nombre_rs)
+        infraccion.append(t_infraccion)
+        valor.append(valor_m)
+        acta.append(c_acta)
+        fechaActa.append(fecha_Acta)
+        placa.append(n_placa)
+        servicio.append(t_servicio)
 
-	data = {
-		'Fecha de resolución': fresolucion,
-		'N°': nresolucion,
-		'Nombre o razón social': nombre,
-		'Infracción': infraccion,
-		'Valor': valor,
-		'Acta': acta,
-		'Fecha de acta': fechaActa,
-		'Placa': placa,
-		'Modalidad de servicio': servicio
-	}
+    data = {
+        'Fecha de resolución': fresolucion,
+        'N°': nresolucion,
+        'Nombre o razón social': nombre,
+        'Infracción': infraccion,
+        'Valor': valor,
+        'Acta': acta,
+        'Fecha de acta': fechaActa,
+        'Placa': placa,
+        'Modalidad de servicio': servicio
+    }
 
-	# Crear Filas de datos y cargarlos en la tabla/modelo Resolucion
-	lista_act = []
-	
-	for i in range(len(fresolucion)):
-		# Creación de las filas
-		fila = [fresolucion[i], nresolucion[i], nombre[i], infraccion[i], valor[i], acta[i], fechaActa[i], placa[i], servicio[i]]
-		lista_act.append(fila)
-		
-		# Carga de datos en la tabla/modelo Resolucion
-		resolucion = Resolucion()
-		
-		resolucion.fresolucion = fresolucion[i]
-		resolucion.nresolucion = nresolucion[i]
-		resolucion.nombre = nombre[i]
-		resolucion.infraccion = infraccion[i]
-		resolucion.valor = valor[i]
-		resolucion.acta = acta[i]
-		resolucion.fechaActa = fechaActa[i]
-		resolucion.placa = placa[i]
-		resolucion.servicio = servicio[i]
-  
-		db.session.add(resolucion)
-		db.session.commit()
-  
+    # Crear Filas de datos y cargarlos en la tabla/modelo Resolucion
+    lista_act = []
+
+    for i in range(len(fresolucion)):
+        # Creación de las filas
+        fila = [fresolucion[i], nresolucion[i], nombre[i], infraccion[i],
+                valor[i], acta[i], fechaActa[i], placa[i], servicio[i]]
+        lista_act.append(fila)
+
+        # Carga de datos en la tabla/modelo Resolucion
+        resolucion = Resolucion()
+
+        resolucion.fresolucion = fresolucion[i]
+        resolucion.nresolucion = nresolucion[i]
+        resolucion.nombre = nombre[i]
+        resolucion.infraccion = infraccion[i]
+        resolucion.valor = valor[i]
+        resolucion.acta = acta[i]
+        resolucion.fechaActa = fechaActa[i]
+        resolucion.placa = placa[i]
+        resolucion.servicio = servicio[i]
+        
+        if len(fresolucion[i]) > 0:
+            resolucion.fecha_resolu = convertirfecha(fresolucion[i])
+        
+        if len(fechaActa[i]) > 0:
+            resolucion.fecha_acta = convertirfecha(fechaActa[i])
+
+        db.session.add(resolucion)
+        db.session.commit()
+
     # Renderizar el template cargaresol.html con el contexto de datos: files, datos
-	files_new = [item.replace("resoluciones\\", "") for item in files]
-	return render_template('cargaresol.html', files = files_new, datos=lista_act)
+    files_new = [item.replace("resoluciones\\", "") for item in files]
+    return render_template('cargaresol.html', files=files_new, datos=lista_act)
 
 # Formulario para subir PDF
 @app.route('/subir_pdf', methods=['GET'])
@@ -349,23 +407,26 @@ def subir_pdf():
 # Formulario para subir PDF a la carpeta resoluciones en el servidor
 @app.route('/cargar_pdf', methods=['POST'])
 def cargar_pdf():
-	if 'archivo' not in request.files:
-		return 'No se seleccionó ningún archivo'
+    # Validar si se seleccionaron archivos
+    if 'archivos' not in request.files:
+        return render_template('error_pdf.html')
 
-	archivo = request.files['archivo']
+    archivos = request.files.getlist('archivos')
 
-	if archivo.filename == '':
-		mensaje = 'No se seleccionó ningún archivo'
-		return render_template('result_pdf.html', mensaje=mensaje)
+    # Verificar si se seleccionaron archivos
+    if len(archivos) == 0:
+        return render_template('error_pdf.html')
 
-	if archivo and allowed_file(archivo.filename):
-		filename = secure_filename(archivo.filename)
-		archivo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		mensaje = 'Archivo cargado con éxito'
-		return render_template('result_pdf.html', mensaje=mensaje)
-	else:
-		mensaje = 'Extensión de archivo no permitida'
-		return render_template('result_pdf.html', mensaje=mensaje)
+    # Subir los archivos a la carpeta resoluciones
+    for archivo in archivos:
+        # pdf.save(os.path.join('resoluciones', pdf.filename))
+        print("***")
+        filename = secure_filename(archivo.filename)
+        print(filename)
+        archivo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        mensaje = 'Archivo(s) cargado(s) con éxito'
+
+    return render_template('result_pdf.html', mensaje=mensaje, archivos=archivos)
 
 # Descarga de la tabla de resoluciones
 @app.route('/descargar_excel', methods=['GET'])
@@ -469,7 +530,7 @@ def resoluciones():
 	datos = Resolucion.query.all()
 	session['datos'] = datos
 	
-	tipo = request.form.get('cboFiltro', 'buscar')
+	tipo = request.args.get('cboFiltro', 'buscar')
 	
 	fecha_i = datetime.date.today()
 	fecha_f = datetime.date.today()
@@ -493,6 +554,7 @@ def resoluciones():
 		
 		if filtro == "resol":
 			if fecha_ini and fecha_fin:
+				
 				datos = Resolucion.query.filter(
 						Resolucion.fecha_resolu >= fecha_ini,
 						Resolucion.fecha_resolu <= fecha_fin
@@ -514,13 +576,13 @@ def resoluciones_editar(id):
 	if request.method == 'POST':
 		fec_res = request.form.get('fecha_resol')
 		if fec_res:
-			fec_res = datetime.strptime(request.form.get('fecha_resol'), '%Y-%m-%d').date()
+			fec_res = datetime.datetime.strptime(request.form.get('fecha_resol'), '%Y-%m-%d').date()
 		else:
 			fec_res = None
 		
 		fec_act = request.form.get('fecha_acta')
 		if fec_act:
-			fec_act = datetime.strptime(request.form.get('fecha_acta'), '%Y-%m-%d').date()
+			fec_act = datetime.datetime.strptime(request.form.get('fecha_acta'), '%Y-%m-%d').date()
 		else:
 			fec_act = None
 		
@@ -533,7 +595,6 @@ def resoluciones_editar(id):
 		resol.valor = float(request.form.get('valor'))
 		resol.acta = request.form.get('acta')
 		
-		#resol.fecha_acta = datetime.strptime(request.form.get('fecha_acta'), '%Y-%m-%d').date()
 		resol.fecha_acta = fec_act
 		
 		resol.placa = request.form.get('placa')
